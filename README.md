5650f58ee80ea93784c5b5d4e7506159dc659000
Vladimir Jimenez <allejo@me.com>
2020-09-19 22:08:17 -0700

    Initial commit of full plugin

---
+++ README.md
@@ -0,0 +1,72 @@
+# BZDB Lock
+
+[![GitHub release](https://img.shields.io/github/release/allejo/bzdbLock.svg)](https://github.com/allejo/bzdbLock/releases/latest)
+![Minimum BZFlag Version](https://img.shields.io/badge/BZFlag-v2.4.4+-blue.svg)
+[![License](https://img.shields.io/github/license/allejo/bzdbLock.svg)](LICENSE.md)
+
+A BZFlag plug-in that allows you to restrict which BZDB settings an admin is allowed or not allowed to change.
+
+The plugin has two modes, a "whitelist" or "blacklist" mode. In whitelist mode, only the specified BZDB will allowed be to changed. In blacklist mode, every BZDB variable **except** for those listed will be allowed to be changed.
+
+## Requirements
+
+- C++11
+- BZFS 2.4.4
+
+This plug-in follows [my standard instructions for compiling plug-ins](https://github.com/allejo/docs.allejo.io/wiki/BZFlag-Plug-in-Distribution).
+
+## Usage
+
+### Loading the plug-in
+
+This plugin expects an INI configuration file with either the blacklist or whitelist definition specified.
+
+```
+-loadplugin bzdbLock,/path/to/variables.cfg
+```
+
+### Configuration File
+
+The configuration is a standard INI file using either the `bzdb_blacklist` or `bzdb_whitelist` keys. [A sample configuration](./bzdbLock.cfg) is provided in this repo.
+
+The only important part about these key/value pairs are the keys; the values are ignored and can be anything at the moment.
+
+```ini
+[bzdb_blacklist]
+_worldSize=0
+```
+
+### Custom Slash Commands
+
+| Command | Permission | Description |
+| ------- | ---------- | ----------- |
+| `/set <param>` | setAll | Overrides the command to disable setting locked variables only. |
+| `/reload [all|bzdbLockList]` | setAll | Re-read the configuration file to load the BZDB lock list |
+
+### Inter-Plug-in Communication
+
+This plug-in supports using generic callbacks for inter-plug-in communication. Since this plug-in uses semantic versioning in its name, accessing this plugin via a generic callback is not feasible. For this reason, the plug-in registers a clip field under the name of `allejo/bzdbLock`.
+
+```cpp
+std::string bzdbLock = bz_getclipFieldString("allejo/bzdbLock");
+
+std::string variable = "_worldSize";
+void* data = teamPair;
+int response = bz_callPluginGenericCallback(bzdbLock.c_str(), "isBZDBVarLocked", data);
+```
+
+| Callback Name | Expected Type | Return Type |
+| ------------- | ------------- | ----------- |
+| `isBZDBVarLocked` | `std::string` | An integer representation of a boolean on whether a variable is locked. |
+
+#### Notes
+
+- The value of `-9999` will be returned in the case of an error
+
+#### Warning
+
+- Passing an object of the incorrect type will lead to unexpected behavior (and possibly server crashes?)
+
+## License
+
+[MIT](LICENSE.md)
---
+++ bzdbLock.cpp
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2020 Vladimir "allejo" Jimenez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <set>
+
+#include "bzfsAPI.h"
+#include "plugin_config.h"
+
+// Define plugin name
+const std::string PLUGIN_NAME = "BZDB Lock List";
+
+// Define plugin version numbering
+const int MAJOR = 1;
+const int MINOR = 0;
+const int REV = 0;
+const int BUILD = 1;
+
+typedef std::pair<std::string, std::string> KeyValue;
+typedef std::vector<KeyValue> KeyValuePairs;
+
+class BZDBLock : public bz_Plugin, public bz_CustomSlashCommandHandler
+{
+public:
+    virtual const char* Name();
+    virtual void Init(const char* config);
+    virtual void Cleanup();
+    virtual int GeneralCallback(const char* name, void* data);
+    virtual void Event(bz_EventData* eventData) {};
+    virtual bool SlashCommand(int playerID, bz_ApiString command, bz_ApiString /*message*/, bz_APIStringList *params);
+
+private:
+    bool isLocked(std::string variable);
+    void loadConfiguration();
+    void loadWatchList(KeyValuePairs sectionName, bool whitelistMode);
+
+    std::string configPath;
+    std::set<std::string> bzdbWatchList = std::set<std::string>();
+    bool whitelistMode = false;
+};
+
+BZ_PLUGIN(BZDBLock)
+
+const char* BZDBLock::Name()
+{
+    static const char* pluginBuild;
+
+    if (!pluginBuild)
+    {
+        pluginBuild = bz_format("%s %d.%d.%d (%d)", PLUGIN_NAME.c_str(), MAJOR, MINOR, REV, BUILD);
+    }
+
+    return pluginBuild;
+}
+
+void BZDBLock::Init(const char* config)
+{
+    // Namespace our clip fields to avoid plug-in conflicts
+    bz_setclipFieldString("allejo/bzdbLock", Name());
+
+    bz_registerCustomSlashCommand("reload", this);
+    bz_registerCustomSlashCommand("set", this);
+
+    if (strlen(config) == 0)
+    {
+        bz_debugMessagef(0, "ERROR :: %s :: This plugin requires a configuration file.", PLUGIN_NAME.c_str());
+    }
+
+    configPath = config;
+
+    loadConfiguration();
+}
+
+void BZDBLock::Cleanup()
+{
+    Flush();
+
+    bz_removeCustomSlashCommand("reload");
+    bz_removeCustomSlashCommand("set");
+}
+
+int BZDBLock::GeneralCallback(const char* name, void* data)
+{
+    if (!name)
+    {
+        return -9999;
+    }
+
+    std::string callback = name;
+
+    if (callback == "isBZDBVarLocked")
+    {
+        auto variable = static_cast<std::string*>(data);
+
+        return (bool)isLocked(*variable);
+    }
+
+    return -9999;
+}
+
+bool BZDBLock::SlashCommand(int playerID, bz_ApiString command, bz_ApiString /*message*/, bz_APIStringList *params)
+{
+    if (command == "reload" && bz_hasPerm(playerID, "setAll"))
+    {
+        if (params->size() == 0)
+        {
+            loadConfiguration();
+        }
+        else if (params->get(0) == "bzdbLockList")
+        {
+            loadConfiguration();
+            bz_sendTextMessage(BZ_SERVER, playerID, "BZDB lock list reloaded");
+
+            return true;
+        }
+
+        return false;
+    }
+    else if (command == "set")
+    {
+        if (isLocked(params->get(0)))
+        {
+            bz_sendTextMessagef(BZ_SERVER, playerID, "The %s BZDB variable has been locked by the server owner and cannot be changed.", params->get(0).c_str());
+
+            return true;
+        }
+
+        return false;
+    }
+
+    return false;
+}
+
+bool BZDBLock::isLocked(std::string variable)
+{
+    auto location = bzdbWatchList.find(bz_tolower(variable.c_str()));
+
+    if (whitelistMode)
+    {
+        return location == bzdbWatchList.end();
+    }
+
+    return location != bzdbWatchList.end();
+}
+
+void BZDBLock::loadConfiguration()
+{
+    auto config = PluginConfig(configPath);
+
+    if (config.errors != 0)
+    {
+        bz_debugMessagef(0, "ERROR :: %s :: The configuration file given has errors in it.", PLUGIN_NAME.c_str());
+
+        return;
+    }
+
+    auto blacklist = config.getSectionItems("bzdb_blacklist");
+    auto whitelist = config.getSectionItems("bzdb_whitelist");
+
+    if (blacklist.size() > 0 && whitelist.size() > 0)
+    {
+        bz_debugMessagef(0, "ERROR :: %s :: Only either a blacklist or a whitelist may be used.", PLUGIN_NAME.c_str());
+    }
+    else if (blacklist.size() == 0 && whitelist.size() == 0)
+    {
+        bz_debugMessagef(0, "ERROR :: %s :: You must specify either a blacklist or whitelist.", PLUGIN_NAME.c_str());
+    }
+
+    if (blacklist.size() > 0)
+    {
+        loadWatchList(blacklist, false);
+    }
+    else if (whitelist.size() > 0)
+    {
+        loadWatchList(whitelist, true);
+    }
+}
+
+void BZDBLock::loadWatchList(KeyValuePairs values, bool isWhitelist)
+{
+    whitelistMode = isWhitelist;
+    bzdbWatchList.clear();
+
+    for (auto value : values)
+    {
+        bzdbWatchList.insert(bz_tolower(value.first.c_str()));
+    }
+}
